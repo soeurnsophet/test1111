235767d4b3f7e7bb8c5539a752628b38fc7472dd
soeurn sophet <soeurnsophet@gmail.com>
2026-09-02 16:32:14 +0700

    Fix database seeder to create test user and remove commented code

--- docker/laravel/entrypoint.development.sh
+++ docker/laravel/entrypoint.development.sh
@@ -2,13 +2,13 @@
 set -e
 
 composer install
-wait $!
+
 npm install
-wait $!
+
 php artisan key:generate
-wait $!
+
 php artisan migrate
-# wait $!
+
 # php artisan storage:link
-wait $!
-php artisan serve --host=0.0.0.0 --port=8000
+
+exec php artisan serve --host=0.0.0.0 --port=8000
